5a40163dab2e7fb754594f602ecc1073207def2c
mccvliqht <micahanaya09@gmail.com>
2025-10-27 21:46:38 +0800

    mobile start pa lang pero no layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
 import { useState } from "react";
+import useDevice from "./useDevice";
 import Header from "./components/header";
 import CameraSection from "./components/cam-section";
 import OutputSection from "./components/output-section";
 import Settings from "./components/settings";
+import MobileLayout from "./components/mobile-layout";
 import "./styles.css";
 
 function App() {
+  const isMobile = useDevice(); 
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
 
-  // SAVED (applied only when the user clicks Save Changes)
+  // SAVED SETTINGS
   const [selectedLang, setSelectedLang] = useState("FSL");
   const [selectedOutput, setSelectedOutput] = useState("Text");
   const [selectedTheme, setSelectedTheme] = useState("Light");
   const [fontSize, setFontSize] = useState(16);
 
+  // MOBILE LAYOUT
+  if (isMobile) {
+    return (
+      <div
+        className={`app mobile ${
+          selectedTheme === "Dark" ? "dark-mode" : "light-mode"
+        }`}
+      >
+        <main className="main-content">
+          <MobileLayout/>
+        </main>
+      </div>
+    );
+  }
+
+  // DESKTOP DEFAULT UI
   return (
-    // root container with theme class -> CSS variables will style the whole page
-    <div className={`app ${selectedTheme === "Dark" ? "dark-mode" : "light-mode"}`}>
+    <div
+      className={`app ${selectedTheme === "Dark" ? "dark-mode" : "light-mode"}`}
+    >
       <Header onOpenSettings={() => setIsSettingsOpen(true)} />
 
       <main className="main-content">
         <CameraSection />
-        <OutputSection lang={selectedLang} output={selectedOutput} fontSize={fontSize} />
+        <OutputSection
+          lang={selectedLang}
+          output={selectedOutput}
+          fontSize={fontSize}
+        />
       </main>
 
       {isSettingsOpen && (
@@ -32,7 +56,6 @@ function App() {
           savedTheme={selectedTheme}
           savedFontSize={fontSize}
           onSave={(lang, output, theme, font) => {
-            // apply all at once only when Save Changes pressed
             setSelectedLang(lang);
             setSelectedOutput(output);
             setSelectedTheme(theme);
---
+++ src/components/mobile-layout.jsx
@@ -0,0 +1,4 @@
+function MobileLayout() {}
+
+
+export default MobileLayout;
